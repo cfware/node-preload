2ad44481539bcdf258a203b14c6f9cf6ba94dbc5
Corey Farrell <git@cfware.com>
2019-10-07 18:58:40 -0400

    fix: Properly handle backslashes in paths on Node.js 12 (#2)

--- test/modern-require.js
+++ test/modern-require.js
@@ -17,11 +17,11 @@ test('exports', async t => {
 });
 
 test('generateRequire', async t => {
-	t.is(generateRequire(noSpaceFile), `--require "${noSpaceFile}"`);
-	t.is(generateRequire(spaceFile), `--require "${spaceFile}"`);
+	t.is(generateRequire(noSpaceFile), `--require "${noSpaceFile.replace(/\\/g, '\\\\')}"`);
+	t.is(generateRequire(spaceFile), `--require "${spaceFile.replace(/\\/g, '\\\\')}"`);
 
 	if (process.platform !== 'win32') {
 		const quoteFile = path.resolve('/dir/file"quote"nospace.js');
-		t.is(generateRequire(quoteFile), `--require "${quoteFile.replace(/"/g, '\\"')}"`);
+		t.is(generateRequire(quoteFile), `--require "${quoteFile.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"`);
 	}
 });
